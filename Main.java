15608d7669b504f1b9102db2eec4f55775ff70e8
julietwainoi <juliewainoi@gmail.com>
2025-01-09 06:39:01 +0300

    running a student class  from absract Main class

--- Main.java
+++ Main.java
@@ -1,18 +1,28 @@
-// HelloWorld.java
-public class Main {
-    public void MainMethod() {
-        System.out.println("Hello, staticMainMethod!");
-    }
+// abstract class
+abstract class Main {
+    public String name = "jane";
+    public int age = 4;
 
-    static void staticMainMethod() {
-        System.out.println("Hello, staticMainMethod!");
+    public abstract void study();
+
+}
+
+class Student extends Main {
+    private int id = 3;
+
+    public void study() {
+        System.out.println("Studying all the long");
+
     }
 
     public static void main(String[] args) {
-        staticMainMethod();
 
-        Main myobject = new Main();
-        myobject.MainMethod();
+        Student myobject = new Student();
+        System.out.println("mi id is: " + myobject.id);
+        System.out.println("my name is:" + myobject.name);
+        System.out.println("my age is:" + myobject.age);
+        myobject.study();
+
     }
 
 }
